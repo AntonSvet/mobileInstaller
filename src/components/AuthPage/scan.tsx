d1d424bac82358c205eb8da99fffd7eb649ebf09
Anton S <a.svetenkov@mail.ru>
2025-03-14 11:28:12 +0300

    Initial commit42

--- src/components/AuthPage/scan.tsx
+++ src/components/AuthPage/scan.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "./../../hooks/useTheme";
-import { BrowserMultiFormatReader } from "@zxing/library";
+import { BarcodeFormat, BrowserMultiFormatReader, DecodeHintType } from "@zxing/library";
 import { useRef, useState, useEffect } from "react";
 import LogoCube from "../../common/LogoCube/LogoCube";
 import MainScreen from "../MainScreen/MainScreen";
@@ -8,28 +8,49 @@ const AuthPage = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [scannedData, setScannedData] = useState<string | null>(null);
   const [isScanning, setIsScanning] = useState<boolean>(false);
+  const [cameras, setCameras] = useState<MediaDeviceInfo[]>([]);
+  const [selectedCamera, setSelectedCamera] = useState<string | null>(null);
+  const [isCameraActive, setIsCameraActive] = useState(false);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [focusMode, setFocusMode] = useState<"continuous" | "single-shot" | "manual">("continuous");
   const { theme } = useTheme();
+  const hints = new Map<DecodeHintType, any>();
+  hints.set(DecodeHintType.POSSIBLE_FORMATS, [
+    BarcodeFormat.EAN_13, // Формат штрих-кода EAN-13
+    BarcodeFormat.CODE_128, // Формат штрих-кода Code 128
+    BarcodeFormat.UPC_A, // Формат штрих-кода UPC-A
+    BarcodeFormat.QR_CODE, // QR-код
+  ]);
+  hints.set(DecodeHintType.TRY_HARDER, true); // Увеличивает точность сканирования
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const codeReader = new BrowserMultiFormatReader();
+  const codeReader = new BrowserMultiFormatReader(hints);
   useEffect(() => {
     document.body.setAttribute("data-theme", theme);
   }, [theme]);
 
-  const [stream, setStream] = useState<MediaStream | null>(null);
+  const getCameras = async () => {
+    try {
+      await navigator.mediaDevices.getUserMedia({ video: true }); // Запрашиваем доступ
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const cameras = devices.filter((device) => device.kind === "videoinput");
+      setCameras(cameras);
+    } catch (error) {
+      console.error("Ошибка при получении камер:", error);
+    }
+  };
+  /*  useEffect(() => {
+       getCameras();
+     }, []); */
   const startScanning = async () => {
     setIsScanning(true);
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    setStream(stream);
-    applyMacroMode(); // Применяем настройки для макро-режима
-    if (videoRef.current) {
-      videoRef.current.srcObject = stream;
-    }
+
     if (videoRef.current) {
       try {
         await codeReader.decodeFromVideoDevice(null, videoRef.current, (result, err) => {
           if (result) {
             setScannedData(result.getText());
-
+            console.log("Результат сканирования:", result.getText());
             stopScanning();
           }
           if (err && !(err instanceof Error)) {
@@ -43,50 +64,70 @@ const AuthPage = () => {
       }
     }
   };
-  // Применение настроек для макро-режима
-  const applyMacroMode = async () => {
-    if (!stream) return;
 
-    const videoTrack: any = stream.getVideoTracks()[0];
-    const capabilities = videoTrack.getCapabilities() as MediaTrackCapabilities & {
-      zoom?: { min: number; max: number; step: number };
-      focusMode?: string[];
-    };
-
-    // Ручная фокусировка на минимальное расстояние
-    if ("focusMode" in capabilities && capabilities.focusMode?.includes("manual")) {
-      await videoTrack.applyConstraints({
-        advanced: [{ focusMode: "manual", focusDistance: 0.1 }],
-      });
-    }
-
-    // Увеличение
-    if ("zoom" in capabilities) {
-      await videoTrack.applyConstraints({
-        advanced: [{ zoom: 2 }], // Увеличение в 2 раза
-      });
-    }
-  };
   const stopScanning = () => {
     codeReader.reset();
     setIsScanning(false);
     videoRef.current = null;
-    if (stream) {
-      stream.getTracks().forEach((track) => track.stop());
-      setStream(null);
-    }
   };
   useEffect(() => {
     return () => {
       if (codeReader) {
         codeReader.reset();
-        if (stream) {
-          stream.getTracks().forEach((track) => track.stop());
-          setStream(null);
-        }
       }
     };
   }, [codeReader]);
+
+  // Запускаем выбранную камеру
+  const startCamera = async (deviceId: string) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId: { exact: deviceId } },
+      });
+      setStream(stream);
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+
+      const videoTrack = stream.getVideoTracks()[0];
+      const capabilities = videoTrack.getCapabilities() as MediaTrackCapabilities & {
+        zoom?: { min: number; max: number; step: number };
+        focusMode?: string[];
+      };
+
+      const constraints: any = {};
+
+      if ("focusMode" in capabilities && capabilities.focusMode?.includes("continuous")) {
+        constraints.focusMode = "continuous";
+      }
+
+      if (Object.keys(constraints).length > 0) {
+        await videoTrack.applyConstraints({ advanced: [constraints] } as MediaTrackConstraints);
+      }
+      if (videoRef.current) {
+        try {
+          await codeReader.decodeFromVideoDevice(null, videoRef.current, (result, err) => {
+            if (result) {
+              setScannedData(result.getText());
+              console.log("Результат сканирования:", result.getText());
+              stopScanning();
+            }
+            if (err && !(err instanceof Error)) {
+              console.error(err);
+            }
+          });
+        } catch (error) {
+          console.error("Error in QR code scanning:", error);
+          alert(`Error in QR code scanning: ${error}`);
+          stopScanning();
+        }
+      }
+      setIsCameraActive(true);
+      setSelectedCamera(deviceId);
+    } catch (error) {
+      console.error("Ошибка при запуске камеры:", error);
+    }
+  };
 
   // Применение режима фокусировки
   const applyFocusMode = async (mode: "continuous" | "single-shot" | "manual") => {
@@ -101,6 +142,7 @@ const AuthPage = () => {
     const constraints: any = {};
     if ("focusMode" in capabilities && capabilities.focusMode?.includes(mode)) {
       constraints.focusMode = mode;
+      setFocusMode(mode);
 
       if (Object.keys(constraints).length > 0) {
         await videoTrack.applyConstraints({ advanced: [constraints] } as MediaTrackConstraints);
@@ -132,7 +174,6 @@ const AuthPage = () => {
   if (scannedData) {
     return <MainScreen />;
   }
-
   return (
     <header className="app-header">
       <div className="logo-elesta">
@@ -160,6 +201,17 @@ const AuthPage = () => {
           <div className="qr-icon">&#x1F4F7;</div>
           <div className="qr-text">{isScanning ? "Отмена" : "Сканировать QR-код"}</div>
         </button>
+        {isScanning && (
+          <div style={{ bottom: "8%" }} className="qr-scan-button">
+            <button onClick={getCameras}>Обновить список камер</button>
+            {cameras.map((camera) => (
+              <button key={camera.deviceId} onClick={() => startCamera(camera.deviceId)}>
+                {camera.label || `Камера ${camera.deviceId}`}
+              </button>
+            ))}
+          </div>
+        )}
+
         <div style={{ bottom: "1%" }} className="qr-scan-button">
           <button onClick={() => applyFocusMode("continuous")}>Непрерывная фокусировка</button>
           <button onClick={() => applyFocusMode("single-shot")}>Одноразовая фокусировка</button>
